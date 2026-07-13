a400ee24df5a0878bbd9ac863bb4bad47222eae8
Lord mohamad <lord100@gmail.com>
2026-07-14 02:56:45 +0330

    for running json server

--- services/api.ts
+++ services/api.ts
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const client = axios.create({ baseURL: "http://localhost:8001" });
+const client = axios.create({ baseURL: "https://agahishop-api.onrender.com" });
 
 export async function getProducts() {
   const { data } = await client.get("/products");
